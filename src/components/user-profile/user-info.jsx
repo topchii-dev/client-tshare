471c79942cb7ffbf4dad61732245741fae8b799b
topchii-dev <topchii.rxz@gmail.com>
2023-10-12 21:27:37 +0200

    added update user information button

--- src/components/user-profile/user-info.jsx
+++ src/components/user-profile/user-info.jsx
@@ -1,14 +1,16 @@
-import React, { Component, useEffect, useState } from 'react'
-import {URL} from '../../constants';
-import {parseTimestamp} from '../../helpers';
+import React, { useEffect, useState } from 'react';
+import { URL } from '../../constants';
+import { parseTimestamp } from '../../helpers';
 import { useParams } from 'react-router-dom';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Image, Button, Input, Form } from 'semantic-ui-react';  // <-- Added Input and Form imports
 import '../../css/user-profile.css';
 
 const UserInfo = () => {
     const { username } = useParams();
     const user = JSON.parse(localStorage.getItem('user'));
     const [error, setError] = useState(null);
+    const [isFormEditable, setIsFormEditable] = useState(false);
+    
 
     async function getUserInfo() {
         try {
@@ -42,26 +44,68 @@ const UserInfo = () => {
         }
     }
 
-    function buildProfile() {
-        if (user !== null) {
-            return (
-                <div className='profile-card-wrapper'>
+    function handleUserUpdate() {
+        // If the form is currently editable, submit changes
+        // (This is where you'd usually send your data to the backend)
+        if (isFormEditable) {
+            // handle the form submission
+        }
+        
+        // Toggle form editability
+        setIsFormEditable(prevState => !prevState);
+    }
+
+function buildProfile() {
+    if (user !== null) {
+        return (
+            <div className='profile-card-wrapper'>
                 <Card>
                     <Image src='' wrapped ui={false} />
                     <Card.Content>
-                    <Card.Header>{user.username ? user.username : 'Undefined'}</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined {parseTimestamp(user.created_at) ? parseTimestamp(user.created_at) : 'Never joined.'}</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        It's your card! Go ahead and write something about yourself.
-                    </Card.Description>
+                        <Button 
+                            basic 
+                            color='black' 
+                            compact
+                            floated='right'
+                            onClick={handleUserUpdate}
+                        >
+                            {isFormEditable ? 'Submit' : 'Update'}
+                        </Button>
+                        <Card.Header>
+                            {/* Make the field editable based on isFormEditable */}
+                            {isFormEditable ? (
+                                <Input 
+                                    fluid
+                                    transparent 
+                                    defaultValue={user.username}
+                                    style={{border: 'none', outline: 'none'}} 
+                                />
+                            ) : (
+                                user.username ? user.username : 'Undefined'
+                            )}
+                        </Card.Header>
+                        <Card.Meta>
+                            <span className='date'>
+                                Joined {parseTimestamp(user.created_at) ? parseTimestamp(user.created_at) : 'Never joined.'}
+                            </span>
+                        </Card.Meta>
+                        <Card.Description>
+                            {/* Example for the card description */}
+                            {isFormEditable ? (
+                                <Form.TextArea 
+                                    defaultValue="It's your card! Go ahead and write something about yourself."
+                                    style={{border: 'none', outline: 'none', minHeight: 'initial', padding: '0'}} 
+                                />
+                            ) : (
+                                "It's your card! Go ahead and write something about yourself."
+                            )}
+                        </Card.Description>
                     </Card.Content>
                 </Card>
-                </div>
-            )
-        }
+            </div>
+        );
     }
+}
 
     useEffect(() => {
         getUserInfo();
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
+/*
+ * This function expects the date format to be provided like so: 2023-10-07T15:27:35.000000Z
+ * Output would be 07.10.2023.
+ */
 export function parseTimestamp(timestamp) {
   const dateObj = new Date(timestamp);
   const day = String(dateObj.getDate()).padStart(2, '0');
